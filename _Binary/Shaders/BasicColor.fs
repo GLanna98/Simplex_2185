694f2b4752e16ac6487c0e9254aa797a56fa6583
gll1460 <gll1460@IGM2000-13>
2019-02-14 13:25:53 -0500

    E03 Done

--- _Binary/Shaders/BasicColor.fs
+++ _Binary/Shaders/BasicColor.fs
@@ -3,6 +3,7 @@
 in vec3 Color;
 
 uniform vec3 SolidColor = vec3(-1,-1,-1);
+uniform bool Inverted =  false;
 
 out vec4 Fragment;
 
@@ -12,6 +13,12 @@ void main()
 	
 	if(SolidColor.r != -1.0 && SolidColor.g != -1.0 && SolidColor.b != -1.0)
 		Fragment = vec4(SolidColor, 1);
+
+	if (Inverted)
+	{
+		Fragment = vec4(1.0,1.0,1.0,1.0) - Fragment;
+		Fragment[3] = 1.0;
+	}
 
 	return;
 }
